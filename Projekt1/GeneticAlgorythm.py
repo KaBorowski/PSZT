e21816f5e632a779b8caba51f9fb530b33f2d684
Karol Borowski <K.Borowski@stud.elka.pw.edu.pl>
2019-04-09 18:49:45 +0200

    P1 metoda ruletki

--- Projekt1/GeneticAlgorythm.py
+++ Projekt1/GeneticAlgorythm.py
@@ -3,8 +3,8 @@ import math
 
 
 class GeneticAlg:
-    def __init__(self, change, coins, population_length=200, penalty1=1000, penalty2=100, mutation_chance=50,
-                 iteration=1000):
+    def __init__(self, change, coins, population_length=100, penalty1=10, penalty2=1, mutation_chance=10,
+                 iteration=1000, elite=1):
         self.change = change
         self.coins = coins
         self.chromosome_length = len(coins)
@@ -15,6 +15,7 @@ class GeneticAlg:
         self.penalty2 = penalty2
         self.mutation_chance = mutation_chance
         self.iteration = iteration
+        self.elite = elite
 
     def random_gene(self):
         # TODO
@@ -62,27 +63,55 @@ class GeneticAlg:
         return population
 
     def new_population(self, population_p, population_r):
-        # population = population_p
-        # population.append(population_r)
+        population_choice_method = 2
+
         population = []
         for i in range(self.population_length):
             population.append(population_p[i])
             population.append(population_r[i])
 
         population = self.sort_population(population)
-        population = [population[x] for x in range(0, self.population_length)]
+
+        if population_choice_method == 1:  # mi najlepszych osobnikow
+            population = [population[x] for x in range(0, self.population_length)]
+
+        else:  # metoda ruletki
+            tmp_population = []
+            population_size = len(population)
+            for i in range(self.elite):  # Zachowanie elitarnych osobnikow
+                tmp_population.append(population[i])
+                del population[i]
+                population_size = population_size - 1
+
+            tmp = [(1.0 / self.fitness(x), x) for x in population]
+            tmp.reverse()
+            error_sum = 0
+            for i in range(len(tmp)):
+                error_sum = error_sum + tmp[i][0]
+            tmp = [(x[0] / error_sum, x[1]) for x in tmp]
+
+            sum = 0
+            roulette = []
+            for i in range(len(tmp)):
+                sum = sum + tmp[i][0]
+                roulette.append([sum, tmp[i][1]])
+
+            roulette = [(x[0] * 10000, x[1]) for x in roulette]
+
+            for i in range(self.population_length - self.elite):
+                rand = -1
+                item = self.random_chromosome()
+                while rand != -1 and item in tmp_population:
+                    rand = random.randint(0, 9999)
+                    item = next(x[1] for x in roulette if rand <= x[0])
+                tmp_population.append(item)
+
+            population = tmp_population
 
         return population
 
     def cross(self, mother, father):
-        locus_count = 1
-        # locus = []
-        # for i in range(locus_count):
-        #     tmp = 0
-        #     while tmp in locus:
-        #         tmp = random.randint(1, self.chromosome- 1)
-        #     locus.append(tmp)
-        # for i in range(locus_count):
+
         cross_method = 2
         child1 = []
         child2 = []
@@ -99,12 +128,12 @@ class GeneticAlg:
         else:
             parent = True
 
-            locus_count = math.floor(self.chromosome_length/3)
+            locus_count = math.floor(self.chromosome_length / 3)
             locus = []
             for i in range(locus_count.__int__()):
                 tmp = 0
                 while tmp in locus:
-                    random.randint(1, self.chromosome_length-1)
+                    random.randint(1, self.chromosome_length - 1)
             for i in range(self.chromosome_length):
                 if i in locus:
                     parent = not parent
@@ -114,8 +143,6 @@ class GeneticAlg:
                 else:
                     child1.append(father[i])
                     child2.append(mother[i])
-
-
 
         return [child1, child2]
 
@@ -137,9 +164,9 @@ class GeneticAlg:
                 father = 1
                 children = [self.random_chromosome(), self.random_chromosome()]
                 while (mother == father) or (children[0] in population_p) or (children[1] in
-                        population_p):
-                    mother = random.randint(0, self.population_length-1)
-                    father = random.randint(0, self.population_length-1)
+                                                                              population_p):
+                    mother = random.randint(0, self.population_length - 1)
+                    father = random.randint(0, self.population_length - 1)
                     children = self.cross(population_p[mother], population_p[father])
                     children[0] = self.mutate(children[0])
                     children[1] = self.mutate(children[1])
@@ -147,21 +174,16 @@ class GeneticAlg:
                 population_r.append(children[0])
                 population_r.append(children[1])
 
-            # for i in range(0, self.population_length):
-            #     population_r[i] = self.mutate(population_r[i])
             population_p = self.new_population(population_p, population_r)
             if self.fitness(population_p[0]) < self.fitness(best_x):
                 best_x = population_p[0]
 
         print(population_p)
-        # print(population_r)
         return best_x
 
 
 if __name__ == '__main__':
-    ga = GeneticAlg(change=833, coins=[1, 2, 5, 10, 20, 50, 100, 200, 500])
-    # ans = ga.create_population()
-    # print(ga.fitness(ans[0]))
+    ga = GeneticAlg(change=839, coins=[1, 2, 5, 10, 20, 50, 100, 200, 500])
     sum = 0
     ans = ga.run()
     print('ROZKLAD MONET = ')
